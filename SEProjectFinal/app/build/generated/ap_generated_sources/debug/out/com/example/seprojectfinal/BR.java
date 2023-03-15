e8e9b8bd0df7204c055c8637d1db5cfd6efcd589
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-03-15 21:07:01 +0800

    Add files via upload

---
+++ SEProjectFinal/app/build/generated/ap_generated_sources/debug/out/com/example/seprojectfinal/BR.java
@@ -0,0 +1,5 @@
+package com.example.seprojectfinal;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ SEProjectFinal/app/build/generated/data_binding_trigger/debug/com/example/seprojectfinal/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.seprojectfinal;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
